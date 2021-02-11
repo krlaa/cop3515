533d870eae7697ff4dc2cfca81f2f41e2bf33368
Antony <kantony@sclogin1.rc.usf.edu>
2021-02-11 01:03:07 -0500

    added if statements

--- Assignment2/Antony_PracticeExcersice2.c
+++ Assignment2/Antony_PracticeExcersice2.c
@@ -9,18 +9,51 @@ int main()
     int year1;
     int day2;
     int month2;
-    int year1;
+    int year2;
 
-    while(1) {
-    printf("Enter the date: ");
-    scanf("%d/%d/%d",&day1,&month1,year1);
-    if (day1 == 0 && month1 == 0 && year1 == 0 )
+    while (1)
     {
-        break;
-    }
-    
+        printf("Enter the date: ");
+        scanf("%d/%d/%d", &day1, &month1, &year1);
+        if (day1 == 0 && month1 == 0 && year1 == 0)
+        {
+            break;
+        }
+        else
+        {
+            printf("Enter the date: ");
+            scanf("%d/%d/%d", &day2, &month2, &year2);
+
+            if(year1<year2){
+                printf("The first date is earlier");                
+            }
+            else if(year2<year1)
+            {
+                printf("The second date is earlier"); 
+            }
+            else{
+                
+            if(month1<month2){
+                printf("The first date is earlier");                
+            }
+            else if(month2<month1)
+            {
+                printf("The second date is earlier"); 
+            }
+            else{
+                
+            if(day1<day2){
+                printf("The first date is earlier");                
+            }
+            else if(day2<day1)
+            {
+                printf("The second date is earlier"); 
+            }
+            }
+            }
+            
+        }
     }
 
-    
     return 0;
 }
